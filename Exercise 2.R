e3c3198e36215d70474cd1f781bfdf1a77ee415c
tauvinia <tatiana.blumberg@web.de>
2024-05-03 12:09:23 +0200

    commit my stuff

---
+++ Exercise 2.R
@@ -0,0 +1,84 @@
+#Import Data
+library("readr")
+library("sf")
+
+wildschwein_BE <- read_delim("wildschwein_BE_2056.csv", ",")
+
+wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056)
+
+#Getting an overview
+
+difftime_secs <- function(later, now){
+  as.numeric(difftime(later, now, units = "secs"))
+}
+
+now <- wildschwein_BE$DatetimeUTC
+
+later <- lead(now)
+
+#wildschwein_BE$timelag <- difftime_secs(later, now)
+wildschwein_BE
+
+wildschwein_BE <- group_by(wildschwein_BE, TierID)
+
+wildschwein_BE <- mutate(wildschwein_BE, 
+       timelag = difftime_secs(lead(DatetimeUTC), DatetimeUTC))
+
+# why we first use later as lead and then as lag?
+
+# Distance
+
+later <- lag(wildschwein_BE$geometry)
+now <- wildschwein_BE$geometry
+
+st_distance(later, now, by_element = TRUE)  # by_element must be set to TRUE
+
+distance_by_element <- function(later, now){
+  as.numeric(
+    st_distance(later, now, by_element = TRUE)
+  )
+}
+
+steplength <- distance_by_element(later, now)
+
+
+#Deriving distance & speed
+
+
+steplength <- st_distance(later, now, by_element = TRUE)
+
+wildschwein_BE <- mutate(wildschwein_BE, 
+       steplength = distance_by_element(lead(geometry), geometry))
+
+wildschwein_BE <- mutate(wildschwein_BE, 
+       speed = steplength/timelag)
+
+wildschwein_BE 
+
+# Plausibility check
+
+wildschwein_sample <- wildschwein_BE |>
+  filter(TierName == "Sabi") |> 
+  head(100)
+
+library(tmap)
+tmap_mode("view")
+
+tm_shape(wildschwein_sample) + 
+  tm_dots()
+
+#tm_shape(wildschwein_BE) + 
+ # tm_dots()
+
+wildschwein_sample_line <- wildschwein_sample |> 
+  # dissolve to a MULTIPOINT:
+  summarise(do_union = FALSE) |> 
+  st_cast("LINESTRING")
+
+tmap_options(basemaps = "OpenStreetMap")
+
+tm_shape(wildschwein_sample_line) +
+  tm_lines() +
+  tm_shape(wildschwein_sample) + 
+  tm_dots()
+
---
+++ Exercise 2_B.R
@@ -0,0 +1,112 @@
+library("readr")
+library("sf")
+library("dplyr")
+
+difftime_secs <- function(x, y){
+  as.numeric(difftime(x, y, units = "secs"))
+}
+
+distance_by_element <- function(later, now){
+  as.numeric(
+    st_distance(later, now, by_element = TRUE)
+  )
+}
+
+caro <- read_delim("caro60.csv", ",") |>
+  st_as_sf(coords = c("E","N"), crs = 2056) |> 
+  select(DatetimeUTC)
+
+#Task 1: Calculate speed at scale 1
+
+
+caro <- mutate(caro, 
+                         timelag = difftime_secs(lead(DatetimeUTC), lag(DatetimeUTC)))
+
+
+
+caro <- mutate(caro, 
+                         steplength = distance_by_element(lead(geometry), lag(geometry)))
+
+caro <- mutate(caro, 
+                         speed = steplength/timelag)
+
+head(caro)
+
+
+#Task 2: Calculate speed at scale 2
+
+
+caro <- mutate(caro, 
+               timelag2 = difftime_secs(lead(DatetimeUTC, 2), lag(DatetimeUTC, 2)))
+
+caro <- mutate(caro, 
+               steplength2 = distance_by_element(lead(geometry, 2), lag(geometry, 2)))
+
+caro <- mutate(caro, 
+               speed2 = steplength2/timelag2)
+
+caro |> 
+  # drop geometry and select only specific columns
+  # to display relevant data only
+  st_drop_geometry() |> 
+  select(timelag2, steplength2, speed2) |> 
+  head()
+
+
+
+#Task 3: Calculate speed at scale 3
+
+
+caro <- mutate(caro, 
+               timelag3 = difftime_secs(lead(DatetimeUTC, 4), lag(DatetimeUTC, 4)))
+
+caro <- mutate(caro, 
+               steplength3 = distance_by_element(lead(geometry, 4), lag(geometry, 4)))
+
+caro <- mutate(caro, 
+               speed3 = steplength3/timelag3)
+
+caro |> 
+  st_drop_geometry() |> 
+  select(timelag3, steplength3, speed3) |> 
+  head()
+
+
+#Task 4: Compare speed across scales
+
+caro |> 
+  st_drop_geometry() |> 
+  select(DatetimeUTC, speed, speed2, speed3)
+
+library(ggplot2)
+
+ggplot(caro, aes(y = speed)) + 
+  # we remove outliers to increase legibility, analogue
+  # Laube and Purves (2011)
+  geom_boxplot(outliers = FALSE)
+
+
+library(tidyr)
+
+# before pivoting, let's simplify our data.frame
+caro2 <- caro |> 
+  st_drop_geometry() |> 
+  select(DatetimeUTC, speed, speed2, speed3)
+
+caro_long <- caro2 |> 
+  pivot_longer(c(speed, speed2, speed3))
+
+head(caro_long)
+
+#why now tibble is much smaller?
+ggplot(caro_long, aes(name, value)) +
+  # we remove outliers to increase legibility, analogue
+  # Laube and Purves (2011)
+  geom_boxplot(outliers = FALSE)
+
+#A steady decrease in median speed as the temporal analysis scale increases;
+#A decrease in the overall variance in speed as the temporal scale increases;
+#Lower minimum values at the shortest temporal scales;
+
+# we got the same results!
+
